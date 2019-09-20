5b985ff7d6a2cc061b3b48314f300b5955629fac
vae4 <vae4@cornell.edu>
2019-09-20 15:25:19 -0400

    new code

--- sort-test.c
+++ sort-test.c
@@ -3,7 +3,7 @@
 
 void sort( int* x_ptr, int* y_ptr )
 {
-  if ( (*y_ptr) > (*x_ptr) ) {
+  if ( (*y_ptr) < (*x_ptr) ) {
     int temp = *x_ptr;
     *x_ptr   = *y_ptr;
     *y_ptr   = temp;
@@ -13,9 +13,14 @@ void sort( int* x_ptr, int* y_ptr )
 int main( void )
 {
   int a = 5;
-  int b = 9;
+  int b = 9;  
+  int c = 6;
+  int d = 1;
   sort( &a, &b );
+  sort( &c, &d );
   UTST_ASSERT_INT_EQ( a, 5 );
   UTST_ASSERT_INT_EQ( b, 9 );
+  UTST_ASSERT_INT_EQ( c,1 );
+  UTST_ASSERT_INT_EQ( d,6 );
   return 0;
 }
